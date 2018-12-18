2ee3901cce2370bb4d6061a92dc360133cdc6ab5
Matthew Davis <matthew@matthewdavis.io>
2018-12-18 10:34:28 -0500

    export

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of ngxux-mat-dialog
  */
 
+export * from './lib/ngxux-mat-dialog-settings';
+
 export * from './lib/ngxux-mat-dialog.service';
 export * from './lib/ngxux-mat-dialog.component';
 export * from './lib/ngxux-mat-dialog.module';
